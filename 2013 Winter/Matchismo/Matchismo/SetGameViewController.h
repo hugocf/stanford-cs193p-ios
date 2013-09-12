706e83b9cc34d52d65be47a27cd6a9aa60384d2f
Hugo Ferreira <hugo@mindclick.info>
2013-09-12 18:22:17 +0100

    Assignment 2: Add tab bar controller for the Set game

---
+++ 2013 Winter/Matchismo/Matchismo/SetGameViewController.h
@@ -0,0 +1,14 @@
+//
+//  SetGameViewController.h
+//  Matchismo
+//
+//  Created by Hugo Ferreira on 2013/09/12.
+//  Copyright (c) 2013 Mindclick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CardGameViewController.h"
+
+@interface SetGameViewController : CardGameViewController
+
+@end
---
+++ 2013 Winter/Matchismo/Matchismo/SetGameViewController.m
@@ -0,0 +1,13 @@
+//
+//  SetGameViewController.m
+//  Matchismo
+//
+//  Created by Hugo Ferreira on 2013/09/12.
+//  Copyright (c) 2013 Mindclick. All rights reserved.
+//
+
+#import "SetGameViewController.h"
+
+@implementation SetGameViewController
+
+@end
