808e80aac4341cd85b1232114ddfb23e0206a343
Hugo Ferreira <hugo@mindclick.info>
2013-10-09 13:00:44 +0100

    Xcode: TODO & FIXME comments as warnings

--- 2013 Winter/Matchismo/Matchismo/Model/SetCard.h
+++ 2013 Winter/Matchismo/Matchismo/Model/SetCard.h
@@ -25,9 +25,9 @@ typedef enum {
 
 /**
  The Set cards vary in four features: number, symbol, shading, and color.
- 
- // TODO How to make the paragraphs split in Xcode 5 quick help?
- // TODO How to separate the "overview/summary" from the "discussion"?
+
+ // TODO: How to make the paragraphs split in Xcode 5 quick help?
+ // TODO: How to separate the "overview/summary" from the "discussion"?
  A set consists of three cards which satisfy all of these conditions:
  
  - They all have the same number, or they have three different numbers.
