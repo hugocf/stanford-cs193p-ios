3b296016286de5b23f5e28fc4308d76320c70919
Hugo Ferreira <hugo@mindclick.info>
2013-10-18 17:27:30 +0100

    Assignment 3: Add tests for Playing and Set decks

---
+++ 2013 Winter/Matchismo/Matchismo Tests/PlayingDeckTests.m
@@ -0,0 +1,36 @@
+//
+//  PlayingDeckTests.m
+//  Matchismo
+//
+//  Created by Hugo Ferreira on 2013/10/17.
+//  Copyright (c) 2013 Mindclick. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "PlayingDeck.h"
+#import "PlayingCard.h"
+
+#pragma mark - Test Support
+
+@interface PlayingDeck (Test)
+- (NSMutableArray *)cards;
+@end
+
+#pragma mark - Test Suite
+
+@interface PlayingDeckTests : XCTestCase
+
+@end
+
+@implementation PlayingDeckTests
+
+- (void)testFullDeckStartsWithAllTheCards
+{
+    PlayingDeck *fullDeck = [[PlayingDeck alloc] init];
+    NSUInteger cardsInDeck = [fullDeck.cards count];
+    NSUInteger totalSuits = [[PlayingCard validSuits] count];
+    NSUInteger totalRanks = [PlayingCard maxRank];
+    XCTAssertEqual(cardsInDeck, totalSuits * totalRanks);
+}
+
+@end
---
+++ 2013 Winter/Matchismo/Matchismo Tests/SetDeckTests.m
@@ -0,0 +1,38 @@
+//
+//  SetDeckTests.m
+//  Matchismo
+//
+//  Created by Hugo Ferreira on 2013/10/17.
+//  Copyright (c) 2013 Mindclick. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "SetDeck.h"
+#import "SetCard.h"
+
+#pragma mark - Test Support
+
+@interface SetDeck (Test)
+- (NSMutableArray *)cards;
+@end
+
+#pragma mark - Test Suite
+
+@interface SetDeckTests : XCTestCase
+
+@end
+
+@implementation SetDeckTests
+
+- (void)testFullDeckStartsWithAllTheCards
+{
+    SetDeck *fullDeck = [[SetDeck alloc] init];
+    NSUInteger cardsInDeck = [fullDeck.cards count];
+    NSUInteger totalNumbers = 3;
+    NSUInteger totalSymbols = [[SetCard validSymbols] count];
+    NSUInteger totalShadings = CARD_SHADING_TYPE_COUNT;
+    NSUInteger totalColors = CARD_COLOR_TYPE_COUNT;
+    XCTAssertEqual(cardsInDeck, totalNumbers * totalSymbols * totalShadings * totalColors);
+}
+
+@end
