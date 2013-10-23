96607fd462b5b9a8466ae9c59773b7da00cf8652
Hugo Ferreira <hugo@mindclick.info>
2013-10-23 14:08:29 +0100

    Assignment 3: Add tests for set cards

--- 2013 Winter/Matchismo/Matchismo Tests/SetCardTests.m
+++ 2013 Winter/Matchismo/Matchismo Tests/SetCardTests.m
@@ -28,9 +28,19 @@
                                                     color:CardColorRed];
 }
 
-#pragma mark - Standard Operations
+#pragma mark - Helpers
 
-- (void)/*TODO:*/OFF_testCardsAreAlwaysFacingUp
+- (SetCard *)createCardWithNumber:(int)number
+                          shading:(CardShadingType)shading
+                            color:(CardColorType)color
+                           symbol:(NSString *)symbol
+{
+    return [[SetCard alloc] initWithNumber:number symbol:symbol shading:shading color:color];
+}
+
+#pragma mark - Standard Card Operations
+
+-(void)testCardsAreAlwaysFacingUp
 {
     XCTAssertTrue(self.oneSolidRedDiamond.isFaceUp, @"Cards should start face up");
     self.oneSolidRedDiamond.faceup = NO;
@@ -44,6 +54,187 @@
     XCTAssertTrue(self.oneSolidRedDiamond.isUnplayable, @"Cards must be able to be disabled for the game");
 }
 
-#pragma mark - ...
+#pragma mark - Creating Cards
+
+- (void)testCannotCreateCardsWithInvalidNumber
+{
+    SetCard *card = [[SetCard alloc] initWithNumber:0
+                                             symbol:CardSymbolDiamond
+                                            shading:CardShadingSolid
+                                              color:CardColorRed];
+    XCTAssertNil(card, @"Must not be able to create an invalid card");
+}
+
+- (void)testCannotCreateCardsWithInvalidSymbol
+{
+    SetCard *card = [[SetCard alloc] initWithNumber:1
+                                             symbol:@"FooBar"
+                                            shading:CardShadingSolid
+                                              color:CardColorRed];
+    XCTAssertNil(card, @"Must not be able to create an invalid card");
+}
+
+- (void)testCannotCreateCardsWithInvalidShading
+{
+    SetCard *card1 = [[SetCard alloc] initWithNumber:1
+                                              symbol:CardSymbolDiamond
+                                             shading:CARD_SHADING_TYPE_COUNT
+                                               color:CardColorRed];
+    SetCard *card2 = [[SetCard alloc] initWithNumber:1
+                                              symbol:CardSymbolDiamond
+                                             shading:1000
+                                               color:CardColorRed];
+    XCTAssertNil(card1, @"Must not be able to create an invalid card");
+    XCTAssertNil(card2, @"Must not be able to create an invalid card");
+}
+
+- (void)testCannotCreateCardsWithInvalidColor
+{
+    SetCard *card1 = [[SetCard alloc] initWithNumber:1
+                                              symbol:CardSymbolDiamond
+                                             shading:CardShadingSolid
+                                               color:CARD_COLOR_TYPE_COUNT];
+    SetCard *card2 = [[SetCard alloc] initWithNumber:1
+                                              symbol:CardSymbolDiamond
+                                             shading:CardShadingSolid
+                                               color:1000];
+    XCTAssertNil(card1, @"Must not be able to create an invalid card");
+    XCTAssertNil(card2, @"Must not be able to create an invalid card");
+}
+
+#pragma mark - Matching Cards
+
+
+- (void)testSetsCanHaveAllTheSameNumber
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:1 shading:CardShadingOpen    color:CardColorGreen  symbol:CardSymbolOval],
+      [self createCardWithNumber:1 shading:CardShadingStriped color:CardColorPurple symbol:CardSymbolSquiggle],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score > 0, @"Matching card sets must return a positive score");
+}
+
+- (void)testSetsCanHaveAllTheSameShading
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:2 shading:CardShadingSolid color:CardColorGreen  symbol:CardSymbolOval],
+      [self createCardWithNumber:3 shading:CardShadingSolid color:CardColorPurple symbol:CardSymbolSquiggle],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score > 0, @"Matching card sets must return a positive score");
+}
+
+- (void)testSetsCanHaveAllTheSameColor
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:2 shading:CardShadingOpen    color:CardColorRed symbol:CardSymbolOval],
+      [self createCardWithNumber:3 shading:CardShadingStriped color:CardColorRed symbol:CardSymbolSquiggle],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score > 0, @"Matching card sets must return a positive score");
+}
+
+- (void)testSetsCanHaveAllTheSameSymbol
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:2 shading:CardShadingOpen    color:CardColorGreen  symbol:CardSymbolDiamond],
+      [self createCardWithNumber:3 shading:CardShadingStriped color:CardColorPurple symbol:CardSymbolDiamond],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score > 0, @"Matching card sets must return a positive score");
+}
+
+- (void)testSetsCanHaveThreeDifferentNumbers
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:2 shading:CardShadingSolid color:CardColorRed symbol:CardSymbolDiamond],
+      [self createCardWithNumber:3 shading:CardShadingSolid color:CardColorRed symbol:CardSymbolDiamond],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score > 0, @"Matching card sets must return a positive score");
+}
+
+- (void)testSetsCanHaveThreeDifferentShadings
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:1 shading:CardShadingOpen    color:CardColorRed symbol:CardSymbolDiamond],
+      [self createCardWithNumber:1 shading:CardShadingStriped color:CardColorRed symbol:CardSymbolDiamond],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score > 0, @"Matching card sets must return a positive score");
+}
+
+- (void)testSetsCanHaveThreeDifferentColors
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:1 shading:CardShadingSolid color:CardColorGreen  symbol:CardSymbolDiamond],
+      [self createCardWithNumber:1 shading:CardShadingSolid color:CardColorPurple symbol:CardSymbolDiamond],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score > 0, @"Matching card sets must return a positive score");
+}
+
+- (void)testSetsCanHaveThreeDifferentSymbols
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:1 shading:CardShadingSolid color:CardColorRed symbol:CardSymbolOval],
+      [self createCardWithNumber:1 shading:CardShadingSolid color:CardColorRed symbol:CardSymbolSquiggle],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score > 0, @"Matching card sets must return a positive score");
+}
+
+- (void)testSetsCannotHaveTwoOfTheSameNumber
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:2 shading:CardShadingOpen    color:CardColorGreen  symbol:CardSymbolOval],
+      [self createCardWithNumber:2 shading:CardShadingStriped color:CardColorPurple symbol:CardSymbolSquiggle],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score < 0, @"Mismatching card sets must return a negative score");
+}
+
+- (void)testSetsCannotHaveTwoOfTheSameShading
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:2 shading:CardShadingOpen color:CardColorGreen  symbol:CardSymbolOval],
+      [self createCardWithNumber:3 shading:CardShadingOpen color:CardColorPurple symbol:CardSymbolSquiggle],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score < 0, @"Mismatching card sets must return a negative score");
+}
+
+- (void)testSetsCannotHaveTwoOfTheSameColor
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:2 shading:CardShadingOpen    color:CardColorGreen  symbol:CardSymbolOval],
+      [self createCardWithNumber:3 shading:CardShadingStriped color:CardColorGreen symbol:CardSymbolSquiggle],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score < 0, @"Mismatching card sets must return a negative score");
+}
+
+- (void)testSetsCannotHaveTwoOfTheSameSymbol
+{
+    NSArray *otherCards =
+    @[
+      [self createCardWithNumber:2 shading:CardShadingOpen    color:CardColorGreen  symbol:CardSymbolOval],
+      [self createCardWithNumber:3 shading:CardShadingStriped color:CardColorPurple symbol:CardSymbolOval],
+      ];
+    int score = [self.oneSolidRedDiamond match:otherCards];
+    XCTAssert(score < 0, @"Mismatching card sets must return a negative score");
+}
 
 @end
