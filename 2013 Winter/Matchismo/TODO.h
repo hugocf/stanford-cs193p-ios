d828f7080c9bd377a4e75d69b2e01bb2230f433f
Hugo Ferreira <hugo@mindclick.info>
2013-10-23 17:00:23 +0100

    TODO: Add placeholder file for general project notes

---
+++ 2013 Winter/Matchismo/TODO.h
@@ -0,0 +1,6 @@
+/* FIXME: use SEL/@selector(name)/NSStringFromSelector() for SetCard property reflection */
+
+/* TODO: Replace all .count => [ count] */
+
+/* TODO: Refactor FooMatchingGame => FooGame */
+
