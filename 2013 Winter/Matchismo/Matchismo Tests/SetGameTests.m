a7c106ff69382fe962679d990e246cd307ec1856
Hugo Ferreira <hugo@mindclick.info>
2013-10-29 19:48:50 +0000

    Assignment 3: Add placeholder for the set game tests

---
+++ 2013 Winter/Matchismo/Matchismo Tests/SetGameTests.m
@@ -0,0 +1,20 @@
+//
+//  SetGameTests.m
+//  Matchismo
+//
+//  Created by Hugo Ferreira on 2013/10/23.
+//  Copyright (c) 2013 Mindclick. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "SetMatchingGame.h"
+
+@interface SetGameTests : XCTestCase
+
+@end
+
+@implementation SetGameTests
+
+// TODO: add tests for the Set game
+
+@end
