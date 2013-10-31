93a59fdf440d81f145567939866b918bd5f184d8
Hugo Ferreira <hugo@mindclick.info>
2013-10-31 15:37:05 +0000

    Assignment 3: IGNORE: Skip the rest of the assignment
    Not much learning value left in there!

--- 2013 Winter/Matchismo/TODO.h
+++ 2013 Winter/Matchismo/TODO.h
@@ -1,3 +1,6 @@
 
 /* TODO: Maybe test fixture should be is a separate classs? 
  (so test code is for sure uncapable to change the ivar) */
+
+/* IGNORE: Skip the rest of assignment 3. Not much learning value left in there! */
+
