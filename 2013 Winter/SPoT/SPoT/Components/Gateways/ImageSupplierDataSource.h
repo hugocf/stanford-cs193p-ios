310092a440d475d7bad662e9531e772bd82107ec
Hugo Ferreira <hugo@mindclick.info>
2013-11-07 22:36:32 +0000

    Assignment 4: Design the interface for image suppliers

--- 2013 Winter/SPoT/SPoT/Components/Gateways/ImageSupplierDataSource.h
+++ 2013 Winter/SPoT/SPoT/Components/Gateways/ImageSupplierDataSource.h
@@ -7,7 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Tag.h"
 
 @protocol ImageSupplierDataSource <NSObject>
+
+/** @return List of `Tag` objects. */
+- (NSArray *)listAllTags;
+
+/**
+ @param tagsToExclude List of string-evaluated objects defining the ignored tags.
+ @return List of `Tag` objects.
+ */
+- (NSArray *)listTagsExcluding:(NSArray *)tagsToExclude;
+
+/** @return List of `Image` objects. */
+- (NSArray *)fetchMax:(NSUInteger)number imagesWithTag:(Tag *)tag;
+
+/** @return List of `Image` objects. */
+- (NSArray *)fetchMax:(NSUInteger)number imagesBeforeDate:(NSDate *)date;
 
 @end
