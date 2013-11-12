27c60cd37100d3e72c0ea821d16a1d5ad69325b6
Hugo Ferreira <hugo@mindclick.info>
2013-11-12 19:13:46 +0000

    Assignment 4: Extract tags from the Flickr photos

--- 2013 Winter/SPoT/SPoT/Adapters/Gateways/ImageSupplierFlickr.m
+++ 2013 Winter/SPoT/SPoT/Adapters/Gateways/ImageSupplierFlickr.m
@@ -7,7 +7,37 @@
 //
 
 #import "ImageSupplierFlickr.h"
+#import "FlickrFetcher.h"
+
+static NSString * const FlickrTagSeparator = @" ";
 
 @implementation ImageSupplierFlickr
+
+#pragma mark - ImageSupplierDataSource
+
+- (NSArray *)listAllTags
+{
+    NSArray *photos = [FlickrFetcher stanfordPhotos];
+    NSArray *tagsPerPhoto = [photos valueForKey:FLICKR_TAGS];
+    NSArray *allTagsUsed = [[tagsPerPhoto componentsJoinedByString:FlickrTagSeparator]
+                            componentsSeparatedByString:FlickrTagSeparator];
+    NSSet *uniqueTagList = [NSSet setWithArray:allTagsUsed];
+    return [uniqueTagList allObjects];
+}
+
+- (NSArray *)listTagsExcluding:(NSArray *)tagsToExclude;
+{
+    return nil;
+}
+
+- (NSArray *)fetchMax:(NSUInteger)number imagesWithTag:(Tag *)tag;
+{
+    return nil;
+}
+
+- (NSArray *)fetchMax:(NSUInteger)number imagesBeforeDate:(NSDate *)date;
+{
+    return nil;
+}
 
 @end
---
+++ 2013 Winter/SPoT/SPoTTests/FlickrFetcherLearningTests.m
@@ -0,0 +1,27 @@
+//
+//  FlickrFetcherLearningTests.m
+//  SPoT
+//
+//  Created by Hugo Ferreira on 2013/11/12.
+//  Copyright (c) 2013 Mindclick. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "FlickrFetcher.h"
+
+@interface FlickrFetcherLearningTests : XCTestCase
+
+@end
+
+@implementation FlickrFetcherLearningTests
+
+- (void)testStanfordPhotosReturnsTags
+{
+    NSArray *photos = [FlickrFetcher stanfordPhotos];
+    NSArray *tagsPerPhoto = [photos valueForKey:FLICKR_TAGS];
+    NSArray *allTagsUsed = [[tagsPerPhoto componentsJoinedByString:@" "] componentsSeparatedByString:@" "];
+    NSSet *uniqueTagList = [NSSet setWithArray:allTagsUsed];
+    XCTAssert([uniqueTagList count] < [allTagsUsed count], @"Duplicate tags should have disappeared");
+}
+
+@end
