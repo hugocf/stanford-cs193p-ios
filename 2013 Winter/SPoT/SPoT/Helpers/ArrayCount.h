39bb488efd318ee0c06081c69b682ec68d6a6829
Hugo Ferreira <hugo@mindclick.info>
2013-11-13 11:26:25 +0000

    Assignment 4: Import helper macros

---
+++ 2013 Winter/SPoT/SPoT/Helpers/ArrayCount.h
@@ -0,0 +1,5 @@
+/**
+ "Get the number of elements of an array passed by parameter in C?"
+ http://stackoverflow.com/a/19415065/1380781
+ */
+#define C_ARRAY_COUNT(array) (sizeof(array)/sizeof(*(array)))
---
+++ 2013 Winter/SPoT/SPoT/Helpers/Helpers.h
@@ -0,0 +1,14 @@
+//
+//  Helpers.h
+//  SPoT
+//
+//  Created by Hugo Ferreira on 2013/11/13.
+//  Copyright (c) 2013 Mindclick. All rights reserved.
+//
+
+#ifndef SPoT_Helpers
+#define SPoT_Helpers
+
+#import "ArrayCount.h"
+
+#endif
